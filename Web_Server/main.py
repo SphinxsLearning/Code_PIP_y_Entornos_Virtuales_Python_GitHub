ae477aa021b68bd8984d6e29a936fb956dbb41a1
SphinxsLearning <sphinxs.learning@gmail.com>
2023-05-24 10:46:57 -0400

    Experiments with FastAPI

--- Web_Server/main.py
+++ Web_Server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [34, 78, -23]
+
+@app.get("/info")
+def get_list():
+    return {"name": 101010}
+
+@app.get("/cositas", response_class=HTMLResponse)
+def get_list():
+    return '''
+        <h1>Hola soy una pagina</h1>
+        <p>soy un parrafo</p>
+    '''
 
 def run():
     store.get_categories()
